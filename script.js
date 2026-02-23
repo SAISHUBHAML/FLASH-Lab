69d4d585f447359ced606fcaae2e571e30aaba93
SAISHUBHAM LAISETTI <86902423+SAISHUBHAML@users.noreply.github.com>
2026-02-23 11:51:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
+
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("show");
+}
+
+
+
+window.addEventListener("scroll", function () {
+    const reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach(section => {
+        const windowHeight = window.innerHeight;
+        const revealTop = section.getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (revealTop < windowHeight - revealPoint) {
+            section.classList.add("active");
+        }
+    });
+});
+
+
